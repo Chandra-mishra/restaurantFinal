8dbc2b10a7044ee0acfa6001ba54e8d2cdb40c42
Chandra Mishra <Prasadchandra18@gmail.com>
2019-05-24 12:52:21 +0530

    Update menuModel.js

--- model/menuModel.js
+++ model/menuModel.js
@@ -8,7 +8,23 @@ const menuSchema = new mongoose.Schema({
   price:{
       type: Number,
       required:true 
-  }
+  },
+  priceCurrency:{
+      type:String,
+      required:true
+  },
+  item_description:{
+      type:String,
+      required:true
+  },
+  item_avaliable_time:{
+     type: String,
+     required:true
+  },
+     
+      isSpecial:Boolean,
+      subItems:[]
 });
 
 module.exports = mongoose.model('menuInfo',menuSchema);
+menuSchema.set('timestamps', true);
